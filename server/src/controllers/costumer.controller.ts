5b012060ec9cda511a35631d692b6a781a4cf8a7
fauzanapkvr <m.fouzan@gt-kw.com>
2025-10-12 20:38:36 +0530

    fix:bug in verfy

--- server/src/controllers/costumer.controller.ts
+++ server/src/controllers/costumer.controller.ts
@@ -105,19 +105,22 @@ class CustomerController {
     }
   }
   async verify(req: Request<{}, {}, LoginRequestBody>, res: Response): Promise<void> {
-   const { password } = req.body;
-    const VALID_PASSWORD = process.env.ADMIN_PASSWORD || "shahul@123";
+  const { password } = req.body;
+  const VALID_PASSWORD = process.env.ADMIN_PASSWORD || "shahul@123";
 
-    if (!password) {
-      return res.status(400).json({ success: false, message: "Password is required" });
-    }
-
-    if (password === VALID_PASSWORD) {
-      return res.status(200).json({ success: true });
-    } else {
-      return res.status(401).json({ success: false, message: "Invalid password" });
-    }
+  if (!password) {
+    res.status(400).json({ success: false, message: "Password is required" });
+    return;
   }
+
+  if (password === VALID_PASSWORD) {
+    res.status(200).json({ success: true });
+    return;
+  }
+
+  res.status(401).json({ success: false, message: "Invalid password" });
+}
+
 }
 
 export default new CustomerController();
